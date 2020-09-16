e639acb75809785d37583cc06e7085860959737d
milos <milos@screensailor.net>
2020-09-16 23:15:03 +0100

    [trunk] park

---
+++ Sources/Lark/DefaultInsertingDictionary.swift
@@ -0,0 +1,53 @@
+@dynamicMemberLookup
+public struct DefaultInsertingDictionary<Key, Value>
+where Key: Hashable
+{
+    public private(set) var dictionary: [Key: Value]
+    public let `default`: () -> Value
+    
+    public init(
+        _ dictionary: [Key: Value] = [:],
+        default ƒ: @escaping @autoclosure () -> Value
+    ) {
+        self.dictionary = dictionary
+        self.default = ƒ
+    }
+}
+
+extension DefaultInsertingDictionary {
+    
+    public subscript(key: Key) -> Value {
+        mutating get {
+            if let value = dictionary[key] {
+                return value
+            } else {
+                let value = self.default()
+                dictionary[key] = value
+                return value
+            }
+        }
+        set {
+            dictionary[key] = newValue
+        }
+    }
+}
+
+extension DefaultInsertingDictionary {
+    
+    public subscript<A>(dynamicMember path: KeyPath<[Key: Value], A>) -> A {
+        dictionary[keyPath: path]
+    }
+    
+    public subscript() -> [Key: Value] {
+        get{ dictionary }
+        set{ dictionary = newValue }
+    }
+}
+
+extension Dictionary {
+    
+    @inlinable
+    public func defaultInserting(default ƒ: @escaping @autoclosure () -> Value) -> DefaultInsertingDictionary<Key, Value> {
+        .init(self, default: ƒ())
+    }
+}
---
+++ Tests/LarkTests/DefaultInsertingDictionary.swift
@@ -0,0 +1,19 @@
+class DefaultInsertingDictionary™: Hopes {
+    
+    func test() {
+        var o = [String: Int]().defaultInserting(default: 3)
+        
+        hope.true(o.isEmpty)
+        
+        o["one"] = 1
+        
+        hope.false(o.isEmpty)
+        
+        o["two"] = 2
+        
+        hope(o.count) == 2
+        
+        hope(o[]) == ["one": 1, "two": 2]
+        hope(o["two"]) == 2
+    }
+}
