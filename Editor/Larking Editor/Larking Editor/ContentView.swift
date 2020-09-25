9a9212b5e0108df5dced98140f3481acba623996
milos <milos@screensailor.net>
2020-09-25 08:56:42 +0100

    park

--- Editor/Larking Editor/Larking Editor/ContentView.swift
+++ Editor/Larking Editor/Larking Editor/ContentView.swift
@@ -10,9 +10,38 @@ import SwiftUI
 struct ContentView: View {
     
     @Binding var document: LarkingDocument
+    
+    @State var commandLine: String = ""
 
     var body: some View {
-        return TextEditor(text: $document.text)
+        // TextEditor(text: $document.text)
+        VStack {
+            Text("above").frame(height: 300)
+            
+            TextField(
+                "Command line",
+                text: $commandLine,
+                onEditingChanged: onEditingChanged,
+                onCommit: onCommit
+            )
+            .lineLimit(nil)
+            .multilineTextAlignment(.center)
+            .textFieldStyle(PlainTextFieldStyle())
+            .padding(8)
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 1))
+            
+            Text("below").frame(height: 300)
+        }
+        .padding()
+        .background(Color.white)
+    }
+    
+    func onEditingChanged(_ x: Bool) {
+        
+    }
+    
+    func onCommit() {
+        
     }
 }
 
@@ -22,3 +51,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+extension NSTextField {
+    
+    open override var focusRingType: NSFocusRingType {
+        get { .none } set {}
+    }
+}
