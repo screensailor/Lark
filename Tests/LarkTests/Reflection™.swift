db4aec478bd686bf7e43496337b0fd8276dbfcf3
milos <milos@screensailor.net>
2020-10-03 21:55:57 +0100

    park

--- Tests/LarkTests/Reflection™.swift
+++ Tests/LarkTests/Reflection™.swift
@@ -25,81 +25,20 @@ class Reflection™: Hopes {
         
         static let breadcrumbs = Reflection.breadcrumbs(in: My.self)
         static let keyPaths = Dictionary(breadcrumbs.map{ ($1, $0) }){ $1 }
+        
+        subscript<A>(breadcrumb: String..., as type: A.Type) -> A? {
+            guard !breadcrumb.isEmpty else { return self as? A }
+            guard let k = My.keyPaths[breadcrumb] else { return nil }
+            return self[keyPath: k] as? A
+        }
     }
 
     func test() throws {
         
         let my = try My.defaultDecodingValue()
         
-        let k = try My.keyPaths[["a", "b", "c", "int"]].hopefully()
-        
-        let bool = my[keyPath: k] as? Int
-        
-        hope(bool) == 0
+        let int = try my["a", "b", "c", "int", as: Int.self].hopefully()
+
+        hope(int) == 0
     }
 }
-
-/*
- ✅ a : A
- ✅ a.b : B
- ✅ a.b.c : C
- ✅ a.b.c.ints : Array<Optional<Int>>
- ✅ a.b.c.ints2 : Optional<Array<Int>>
- ✅ a.b.c.string : Optional<String>
- ✅ a.b.c.bool : Bool
- ✅ a.b.c.url : Optional<URL>
- ✅ a.b.c2 : C
- ✅ a.b.c2.ints : Array<Optional<Int>>
- ✅ a.b.c2.ints2 : Optional<Array<Int>>
- ✅ a.b.c2.string : Optional<String>
- ✅ a.b.c2.bool : Bool
- ✅ a.b.c2.url : Optional<URL>
- ✅ a.b.int : Int
- ✅ a.b2 : B
- ✅ a.b2.c : C
- ✅ a.b2.c.ints : Array<Optional<Int>>
- ✅ a.b2.c.ints2 : Optional<Array<Int>>
- ✅ a.b2.c.string : Optional<String>
- ✅ a.b2.c.bool : Bool
- ✅ a.b2.c.url : Optional<URL>
- ✅ a.b2.c2 : C
- ✅ a.b2.c2.ints : Array<Optional<Int>>
- ✅ a.b2.c2.ints2 : Optional<Array<Int>>
- ✅ a.b2.c2.string : Optional<String>
- ✅ a.b2.c2.bool : Bool
- ✅ a.b2.c2.url : Optional<URL>
- ✅ a.b2.int : Int
- ✅ a.int : Int
- ✅ a2 : A
- ✅ a2.b : B
- ✅ a2.b.c : C
- ✅ a2.b.c.ints : Array<Optional<Int>>
- ✅ a2.b.c.ints2 : Optional<Array<Int>>
- ✅ a2.b.c.string : Optional<String>
- ✅ a2.b.c.bool : Bool
- ✅ a2.b.c.url : Optional<URL>
- ✅ a2.b.c2 : C
- ✅ a2.b.c2.ints : Array<Optional<Int>>
- ✅ a2.b.c2.ints2 : Optional<Array<Int>>
- ✅ a2.b.c2.string : Optional<String>
- ✅ a2.b.c2.bool : Bool
- ✅ a2.b.c2.url : Optional<URL>
- ✅ a2.b.int : Int
- ✅ a2.b2 : B
- ✅ a2.b2.c : C
- ✅ a2.b2.c.ints : Array<Optional<Int>>
- ✅ a2.b2.c.ints2 : Optional<Array<Int>>
- ✅ a2.b2.c.string : Optional<String>
- ✅ a2.b2.c.bool : Bool
- ✅ a2.b2.c.url : Optional<URL>
- ✅ a2.b2.c2 : C
- ✅ a2.b2.c2.ints : Array<Optional<Int>>
- ✅ a2.b2.c2.ints2 : Optional<Array<Int>>
- ✅ a2.b2.c2.string : Optional<String>
- ✅ a2.b2.c2.bool : Bool
- ✅ a2.b2.c2.url : Optional<URL>
- ✅ a2.b2.int : Int
- ✅ a2.int : Int
- ✅ int : Int
-
- */
